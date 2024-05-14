a7c95c699d84afa0a73ba2634a608ed9e55909c0
Geronimo-Freese <geronimo.freese@gmail.com>
2024-05-14 10:36:50 +0200

    Version 1.0 of xorcrypt.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <cmath>
 #include <cstdio>
 #include <iostream>
 #include <string>
@@ -6,72 +5,35 @@
 using std::cin;
 using std::cout;
 using std::endl;
-using std::string;
 
-/**
- * @class Enc_Info
- * @brief Encryption information for the given input.
- *        Should be a singelton so there can't be two instances.
- *
- */
-class Enc_Info {
-private:
-  const string key;
+std::string key = "";
 
-  static Enc_Info *instancePtr;
+void print_usage(void) { cout << "Usage: xorcrypt [KEY]" << endl; }
 
-  static Enc_Info *get_Instance(string key);
-
-  Enc_Info(string arg_key) : key(arg_key) {}
-
-public:
-  /**
-   * @brief may not be cloneable
-   */
-  Enc_Info(Enc_Info &clone) = delete;
-
-  /**
-   * @brief may not be assingable
-   */
-  void operator=(const Enc_Info &) = delete;
-
-  /**
-   * @brief
-   */
-  std::string get_key(void);
-
-  static Enc_Info *get_instance(string key) {
-    if (instancePtr == nullptr) {
-      instancePtr = new Enc_Info(string key);
-      return instancePtr;
-    } else {
-      return instancePtr;
-    }
+void get_key(int argc, char *argv[]) {
+  if (argc < 2) {
+    print_usage();
+    exit(1);
+  } else {
+    key = argv[1];
   }
-};
-
-string parse_encryption_key(char *arg_key) {
-  string key(arg_key);
-
-  return key;
 }
 
 int main(int argc, char *argv[]) {
+  get_key(argc, argv);
+  char c = 0;
+  char c_crypt = 0;
+  int i = 0;
 
-
-  if (argc < 2) {
-    cout << "Please provide an encryption key: ";
-    cin >> key;
-    return 2;
+  while(cin.peek() != EOF){
+    cin.get(c);
+    c_crypt = (c^key[i]);
+    cout << c_crypt;
+    i++;
+    i %= key.size();
   }
 
-  cout << argv[1] << endl;
-  return 0;
-
-  string line = "";
-  while (getline(cin, line)) {
-    cout << line << endl;
-  }
+  cout << EOF;
 
   return 0;
 }
